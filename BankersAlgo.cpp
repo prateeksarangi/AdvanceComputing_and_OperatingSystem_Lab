afb923392d0cfd1d32581da300418d67f747194c
prateeksarangi <sarangiprateek80@gmail.com>
2019-10-18 11:22:52 +0530

    Updated 18/10/19 Banker Algorithm

--- BankersAlgo.cpp
+++ BankersAlgo.cpp
@@ -1,131 +1,74 @@
 #include<iostream> 
-using namespace std; 
-  
-// Number of processes 
+using namespace std;
 const int P = 5; 
-  
-// Number of resources 
 const int R = 3; 
-  
-// Function to find the need of each process 
-void calculateNeed(int need[P][R], int maxm[P][R], 
-                   int allot[P][R]) 
-{ 
-    // Calculating Need of each P 
+
+void calculateNeed(int need[P][R], int maxm[P][R], int allot[P][R]){ 
     for (int i = 0 ; i < P ; i++) 
         for (int j = 0 ; j < R ; j++) 
-  
-            // Need of instance = maxm instance - 
-            //                    allocated instance 
             need[i][j] = maxm[i][j] - allot[i][j]; 
 } 
-  
-// Function to find the system is in safe state or not 
-bool isSafe(int processes[], int avail[], int maxm[][R], 
-            int allot[][R]) 
-{ 
+
+bool isSafe(int processes[], int avail[], int maxm[][R], int allot[][R]){ 
     int need[P][R]; 
-  
-    // Function to calculate need matrix 
     calculateNeed(need, maxm, allot); 
-  
-    // Mark all processes as infinish 
     bool finish[P] = {0}; 
-  
-    // To store safe sequence 
     int safeSeq[P]; 
-  
-    // Make a copy of available resources 
     int work[R]; 
     for (int i = 0; i < R ; i++) 
         work[i] = avail[i]; 
-  
-    // While all processes are not finished 
-    // or system is not in safe state. 
+
     int count = 0; 
-    while (count < P) 
-    { 
-        // Find a process which is not finish and 
-        // whose needs can be satisfied with current 
-        // work[] resources. 
+    while(count < P){ 
         bool found = false; 
-        for (int p = 0; p < P; p++) 
-        { 
-            // First check if a process is finished, 
-            // if no, go for next condition 
-            if (finish[p] == 0) 
-            { 
-                // Check if for all resources of 
-                // current P need is less 
-                // than work 
+        for(int p = 0; p < P; p++){
+            if(finish[p] == 0){ 
                 int j; 
-                for (j = 0; j < R; j++) 
-                    if (need[p][j] > work[j]) 
+                for(j = 0; j < R; j++) 
+                    if(need[p][j] > work[j]) 
                         break; 
   
-                // If all needs of p were satisfied. 
-                if (j == R) 
-                { 
-                    // Add the allocated resources of 
-                    // current P to the available/work 
-                    // resources i.e.free the resources 
+                if(j == R){ 
                     for (int k = 0 ; k < R ; k++) 
                         work[k] += allot[p][k]; 
-  
-                    // Add this process to safe sequence. 
-                    safeSeq[count++] = p; 
-  
-                    // Mark this p as finished 
+
+                    safeSeq[count++] = p;  
                     finish[p] = 1; 
-  
                     found = true; 
                 } 
             } 
         } 
-  
-        // If we could not find a next process in safe 
-        // sequence. 
-        if (found == false) 
-        { 
+
+        if (found == false){ 
             cout << "System is not in safe state"; 
             return false; 
         } 
     } 
-  
-    // If system is in safe state then 
-    // safe sequence will be as below 
+
     cout << "System is in safe state.\nSafe"
          " sequence is: "; 
     for (int i = 0; i < P ; i++) 
-        cout << safeSeq[i] << " "; 
+        cout << safeSeq[i]+1 << " "; 
+    cout<<endl;
   
     return true; 
 } 
   
-// Driver code 
+
 int main() 
 { 
     int processes[] = {0, 1, 2, 3, 4}; 
-  
-    // Available instances of resources 
-    int avail[] = {3, 3, 2}; 
-  
-    // Maximum R that can be allocated 
-    // to processes 
-    int maxm[][R] = {{7, 5, 3}, 
-                     {3, 2, 2}, 
-                     {9, 0, 2}, 
-                     {2, 2, 2}, 
+    int avail[] = {15, 8, 8}; 
+    int maxm[][R] = {{5, 6, 3}, 
+                     {8, 5, 6}, 
+                     {4, 9, 2}, 
+                     {7, 4, 3}, 
                      {4, 3, 3}}; 
-  
-    // Resources allocated to processes 
-    int allot[][R] = {{0, 1, 0}, 
-                      {2, 0, 0}, 
+    int allot[][R] = {{2, 1, 0}, 
+                      {3, 2, 3}, 
                       {3, 0, 2}, 
-                      {2, 1, 1}, 
-                      {0, 0, 2}}; 
-  
-    // Check system is in safe state or not 
+                      {3, 2, 0}, 
+                      {1, 0, 1}}; 
     isSafe(processes, avail, maxm, allot); 
   
     return 0; 
