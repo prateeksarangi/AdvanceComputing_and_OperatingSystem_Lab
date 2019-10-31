6fdb18b300128bcb10f4d129fdc67add87809ea4
Prateek Sarangi <sarangiprateek80@gmail.com>
2019-10-31 15:43:26 +0530

    Add files via upload

---
+++ deffie.py
@@ -0,0 +1,137 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo1.py =======================================
+147
+66
+75
+75
+³´¾k´¾k¬kÁ°½Äk¾°®½°¿k¸°¾¾¬²°lll
+This is a very secret message!!!
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Publicly Shared Prime:  23
+    Publicly Shared Base:   5
+
+  Alice Sends Over Public Chanel:  8
+&amp;amp;amp;amp;amp;amp;quot;   Bob Sends Over Public Chanel:  19
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Publicly Shared Prime: 55
+    Publicly Shared Base:   5
+
+  Alice Sends Over Public Chanel:  5
+&amp;amp;amp;amp;amp;amp;quot;   Bob Sends Over Public Chanel:  45
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  45
+    Bob Shared Secret:  45
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Publicly Shared Prime: 59
+    Publicly Shared Base:  3
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py", line 13, in <module>
+    A = (sharedBase**aliceSecret) % sharedPrime
+NameError: name 'aliceSecret' is not defined
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Publicly Shared Prime: 23
+    Publicly Shared Base:  5
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py", line 13, in <module>
+    A = (sharedBase**aliceSecret) % sharedPrime
+NameError: name 'aliceSecret' is not defined
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Publicly Shared Prime: 23
+    Publicly Shared Base:  5
+Secret message of Alice: 6
+Secret message of Bob: 15
+
+  Alice Sends Over Public Chanel:  8
+&amp;amp;amp;amp;amp;amp;quot;   Bob Sends Over Public Chanel:  19
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Enter the value of 'n': 23
+    Enter the value of 'g':  5
+Secret message of Alice: 6
+Secret message of Bob: 15
+
+  Alice Sends Over Public Chanel:  8
+&amp;amp;amp;amp;amp;amp;quot;   Bob Sends Over Public Chanel:  19
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Enter the value of 'n': 23
+    Enter the value of 'g':  5
+Secret message of Alice: 6
+Secret message of Bob: 15
+
+  Alice Sends Over Public Chanel:  8
+   Bob Sends Over Public Chanel:  19
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Enter the value of 'n': 23
+    Enter the value of 'g':  5
+Secret message of Alice: 6
+Secret message of Bob: 15
+
+  Alice Sends Over Public Chanel:  8
+  Bob Sends Over Public Chanel:  19
+
+------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
+======================================== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python37/DiffieHellmanAlgo.py ========================================
+Publicly Shared Variables:
+    Enter the value of 'n': 23
+    Enter the value of 'g':  5
+Secret message of Alice: 6
+Secret message of Bob: 15
+
+  Alice Sends Over Public Chanel:  8
+  Bob Sends Over Public Chanel:  19
+
+------------------------
+
+Privately Calculated Shared Secret:
+    Alice Shared Secret:  2
+    Bob Shared Secret:  2
+>>> 
